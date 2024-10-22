9cd62267c90c83253715969a38afd6ae2744dd5c
jkais-jks <147993417+RLGen@users.noreply.github.com>
2024-10-22 10:42:52 +0800

    Update image_model.py

--- model/image_model.py
+++ model/image_model.py
@@ -1,68 +1,129 @@
-import math
 import torch
 import torch.nn as nn
-import torch.nn.init as init 
 import torch.nn.functional as F
-import torch.optim as optim
+import torch.nn.init as init
 
-def call_bn(bn, x):
-    return bn(x)
+from torch.autograd import Variable
 
-class CNN(nn.Module):
-    def __init__(self, input_channel=3, n_outputs=10, dropout_rate=0.25, top_bn=False):
-        self.dropout_rate = dropout_rate
-        self.top_bn = top_bn
-        super(CNN, self).__init__()
-        self.c1=nn.Conv2d(input_channel,128,kernel_size=3,stride=1, padding=1)
-        self.c2=nn.Conv2d(128,128,kernel_size=3,stride=1, padding=1)
-        self.c3=nn.Conv2d(128,128,kernel_size=3,stride=1, padding=1)
-        self.c4=nn.Conv2d(128,256,kernel_size=3,stride=1, padding=1)
-        self.c5=nn.Conv2d(256,256,kernel_size=3,stride=1, padding=1)
-        self.c6=nn.Conv2d(256,256,kernel_size=3,stride=1, padding=1)
-        self.c7=nn.Conv2d(256,512,kernel_size=3,stride=1, padding=0)
-        self.c8=nn.Conv2d(512,256,kernel_size=3,stride=1, padding=0)
-        self.c9=nn.Conv2d(256,128,kernel_size=3,stride=1, padding=0)
-        self.l_c1=nn.Linear(128,n_outputs)
-        self.bn1=nn.BatchNorm2d(128)
-        self.bn2=nn.BatchNorm2d(128)
-        self.bn3=nn.BatchNorm2d(128)
-        self.bn4=nn.BatchNorm2d(256)
-        self.bn5=nn.BatchNorm2d(256)
-        self.bn6=nn.BatchNorm2d(256)
-        self.bn7=nn.BatchNorm2d(512)
-        self.bn8=nn.BatchNorm2d(256)
-        self.bn9=nn.BatchNorm2d(128)
+__all__ = ['ResNet_s', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202']
 
-    def forward(self, x,):
-        h=x
-        h=self.c1(h)
-        h=F.leaky_relu(call_bn(self.bn1, h), negative_slope=0.01)
-        h=self.c2(h)
-        h=F.leaky_relu(call_bn(self.bn2, h), negative_slope=0.01)
-        h=self.c3(h)
-        h=F.leaky_relu(call_bn(self.bn3, h), negative_slope=0.01)
-        h=F.max_pool2d(h, kernel_size=2, stride=2)
-        h=F.dropout2d(h, p=self.dropout_rate)
+def _weights_init(m):
+    classname = m.__class__.__name__
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
 
-        h=self.c4(h)
-        h=F.leaky_relu(call_bn(self.bn4, h), negative_slope=0.01)
-        h=self.c5(h)
-        h=F.leaky_relu(call_bn(self.bn5, h), negative_slope=0.01)
-        h=self.c6(h)
-        h=F.leaky_relu(call_bn(self.bn6, h), negative_slope=0.01)
-        h=F.max_pool2d(h, kernel_size=2, stride=2)
-        h=F.dropout2d(h, p=self.dropout_rate)
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
 
-        h=self.c7(h)
-        h=F.leaky_relu(call_bn(self.bn7, h), negative_slope=0.01)
-        h=self.c8(h)
-        h=F.leaky_relu(call_bn(self.bn8, h), negative_slope=0.01)
-        h=self.c9(h)
-        h=F.leaky_relu(call_bn(self.bn9, h), negative_slope=0.01)
-        h=F.avg_pool2d(h, kernel_size=h.data.shape[2])
+    def forward(self, x):
+        return self.lambd(x)
 
-        h = h.view(h.size(0), h.size(1))
-        logit=self.l_c1(h)
-        if self.top_bn:
-            logit=call_bn(self.bn_c1, logit)
-        return logit
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, option='A', input_size=3):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=input_size, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            if option == 'A':
+                """
+                For CIFAR10 ResNet paper uses option A.
+                """
+                self.shortcut = LambdaLayer(lambda x:
+                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
+            elif option == 'B':
+                self.shortcut = nn.Sequential(
+                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     nn.BatchNorm2d(self.expansion * planes)
+                )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet_s(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet_s, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.linear = nn.Linear(64, num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = F.avg_pool2d(out, out.size()[3])
+        feats = out.view(out.size(0), -1)
+        out = self.linear(feats)
+        return out, feats
+
+
+def resnet20():
+    return ResNet_s(BasicBlock, [3, 3, 3])
+
+
+def resnet32(num_classes):
+    return ResNet_s(BasicBlock, [5, 5, 5], num_classes=num_classes)
+
+
+def resnet44():
+    return ResNet_s(BasicBlock, [7, 7, 7])
+
+
+def resnet56():
+    return ResNet_s(BasicBlock, [9, 9, 9])
+
+
+def resnet110():
+    return ResNet_s(BasicBlock, [18, 18, 18])
+
+
+def resnet1202():
+    return ResNet_s(BasicBlock, [200, 200, 200])
+
+
+def test(net):
+    import numpy as np
+    total_params = 0
+
+    for x in filter(lambda p: p.requires_grad, net.parameters()):
+        total_params += np.prod(x.data.numpy().shape)
+    print("Total number of params", total_params)
+    print("Total layers", len(list(filter(lambda p: p.requires_grad and len(p.data.size())>1, net.parameters()))))
+
+
+if __name__ == "__main__":
+    for net_name in __all__:
+        if net_name.startswith('resnet'):
+            print(net_name)
+            test(globals()[net_name]())
+            print()
