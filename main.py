8cf49920d5160a31e811a01e279a1151b90a220e
111 <147993417+RLGen@users.noreply.github.com>
2024-10-23 20:03:00 +0800

    .

--- main.py
+++ main.py
@@ -5,18 +5,340 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
+import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
+from torch.autograd import grad
+import os
+import random
+import time
+import warnings
+import models
+import sys
+import numpy as np
 
 import argparse, sys
 import numpy as np
 import datetime
 import shutil
+from fl import FacilityLocationCIFAR
+from lazyGreedy import lazy_greedy_heap
 
 # IMPORT DATASET AND LOSS FROM UTILS\
 from parser.get_parser import get_parser
 from utils import *
+from model.image_model import *
+from model.table_model import *
+import time
+
+def main():
+    args = parser.parse_args()
+    if args.use_crust:
+        args.store_name = '_'.join([args.dataset, args.arch, args.mislabel_type, str(args.mislabel_ratio), str(args.fl_ratio), str(args.r), args.exp_str])
+    else:
+        args.store_name = '_'.join([args.dataset, args.arch, args.mislabel_type, str(args.mislabel_ratio), args.exp_str])
+    prepare_folders(args)
+    #wandb.init(project="robust_cifar", tensorboard=True, name=args.store_name)
+    #wandb.config.update(args)
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
+
+    if args.gpu is not None:
+        warnings.warn('You have chosen a specific GPU. This will completely '
+                      'disable data parallelism.')
+
+    ngpus_per_node = torch.cuda.device_count()
+    main_worker(args.gpu, ngpus_per_node, args)
+
+
+def main_worker(gpu, ngpus_per_node, args):
+    global best_acc1
+    args.gpu = gpu
+
+    if args.gpu is not None:
+        print("Use GPU: {} for training".format(args.gpu))
+
+    # create model
+    print("=> creating model '{}'".format(args.arch))
+    args.num_classes = 100 if args.dataset == 'cifar100' else 10
+    model = models.__dict__[args.arch](num_classes=args.num_classes)
+
+    if args.gpu is not None:
+        torch.cuda.set_device(args.gpu)
+        model = model.cuda(args.gpu)
+    else:
+        # DataParallel will divide and allocate batch_size to all available GPUs
+        model = torch.nn.DataParallel(model).cuda()
+
+    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_acc1 = checkpoint['best_acc1']
+            if args.gpu is not None:
+                # best_acc1 may be from a checkpoint from a different GPU
+                best_acc1 = best_acc1.to(args.gpu)
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    cudnn.benchmark = True
+
+    # Data loading code
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_val = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+
+    criterion = nn.CrossEntropyLoss(reduction='none').cuda(args.gpu)
+        
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    trainval_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    if args.evaluate:
+        validate(val_loader, model, criterion, 0, args)
+        return
+
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
+            milestones=[80, 100], last_epoch=args.start_epoch - 1)
+
+    # init log for training
+    log_training = open(os.path.join(args.root_log, args.store_name, 'log.csv'), 'w')
+    with open(os.path.join(args.root_log, args.store_name, 'args.txt'), 'w') as f:
+        f.write(str(args))
+    tf_writer = SummaryWriter(log_dir=os.path.join(args.root_log, args.store_name))
+    weights = [1] * len(train_dataset)
+    weights = torch.FloatTensor(weights)
+    for epoch in range(args.start_epoch, args.epochs):
+        
+        if args.use_crust and epoch >= 5:
+            train_dataset.switch_data()         
+            # FL part
+            grads_all, labels = estimate_grads(trainval_loader, model, criterion, args, epoch, log_training)
+            # per-class clustering
+            ssets = []
+            weights = []
+            for c in range(args.num_classes):
+                sample_ids = np.where((labels == c) == True)[0]
+                grads = grads_all[sample_ids]
+                
+                dists = pairwise_distances(grads)
+                weight = np.sum(dists < args.r, axis=1)
+                V = range(len(grads))
+                F = FacilityLocationCIFAR(V, D=dists)
+                B = int(args.fl_ratio * len(grads))
+                sset, vals = lazy_greedy_heap(F, V, B)
+                weights.extend(weight[sset].tolist())
+                sset = sample_ids[np.array(sset)]
+                ssets += list(sset)
+            weights = torch.FloatTensor(weights)
+            train_dataset.adjust_base_indx_tmp(ssets)
+            label_acc = train_dataset.estimate_label_acc()
+            tf_writer.add_scalar('label_acc', label_acc, epoch)
+            log_training.write('epoch %d label acc: %f\n'%(epoch, label_acc))
+            print("change train loader")
+            
+        # train for one epoch
+        if args.use_crust and epoch > 5:
+            train(train_loader, model, criterion, weights, optimizer, epoch, args, log_training, tf_writer, fetch=True)
+        else:
+            train(train_loader, model, criterion, weights, optimizer, epoch, args, log_training, tf_writer, fetch=False)
+        
+        # evaluate on validation set
+        acc1 = validate(val_loader, model, criterion, epoch, args, log_training, tf_writer)
+
+        # remember best acc@1 and save checkpoint
+        is_best = acc1 > best_acc1
+        best_acc1 = max(acc1, best_acc1)
+
+        tf_writer.add_scalar('acc/test_top1_best', best_acc1, epoch)
+        output_best = 'Best Prec@1: %.3f\n' % (best_acc1)
+        print(output_best)
+
+        save_checkpoint(args, {
+            'epoch': epoch + 1,
+            'arch': args.arch,
+            'state_dict': model.state_dict(),
+            'best_acc1': best_acc1,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best)
+        lr_scheduler.step()
+
+    print('best_acc1: {:.4f}'.format(best_acc1.item()))
+
+
+def train(train_loader, model, criterion, weights, optimizer, epoch, args, log_training, tf_writer, fetch=False):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(len(train_loader), batch_time, data_time, losses, top1,
+                             top5, prefix="Epoch: [{}]".format(epoch))
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, batch in enumerate(train_loader):
+        input, target, target_real, index = batch
+        if fetch:
+            input_b = train_loader.dataset.fetch(target)
+            lam = np.random.beta(1, 0.1)
+            input = lam * input + (1 - lam) * input_b
+        c_weights = weights[index]
+        c_weights = c_weights.type(torch.FloatTensor)
+        c_weights =  c_weights / c_weights.sum()
+        if args.gpu is not None:
+            c_weights = c_weights.to(args.gpu, non_blocking=True)
+    
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        input = input.type(torch.FloatTensor)
+        if args.gpu is not None:
+            input = input.cuda(args.gpu, non_blocking=True)
+            target = target.cuda(args.gpu, non_blocking=True)
+
+        # compute output
+        output, feats = model(input)
+        loss = criterion(output, target)
+        loss = (loss * c_weights).sum()
+        
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(acc1[0], input.size(0))
+        top5.update(acc5[0], input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            progress.print(i)
+
+    tf_writer.add_scalar('loss/train', losses.avg, epoch)
+    tf_writer.add_scalar('acc/train_top1', top1.avg, epoch)
+    tf_writer.add_scalar('acc/train_top5', top5.avg, epoch)
+    tf_writer.add_scalar('lr', optimizer.param_groups[-1]['lr'], epoch)
+
+def validate(val_loader, model, criterion, epoch, args, log_training=None, tf_writer=None):
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(len(val_loader), batch_time, losses, top1, top5,
+                             prefix='Test: ')
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (input, target) in enumerate(val_loader):
+            input = input.type(torch.FloatTensor)
+            if args.gpu is not None:
+                input = input.cuda(args.gpu, non_blocking=True)
+                target = target.cuda(args.gpu, non_blocking=True)
+
+            # compute output
+            output, feats = model(input)
+            loss = criterion(output, target)
+            loss = loss.mean()
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), input.size(0))
+            top1.update(acc1[0], input.size(0))
+            top5.update(acc5[0], input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                progress.print(i)
+
+        # TODO: this should also be done with the ProgressMeter
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
+
+        if tf_writer is not None:
+            tf_writer.add_scalar('loss/test', losses.avg, epoch)
+            tf_writer.add_scalar('acc/test_top1', top1.avg, epoch)
+            tf_writer.add_scalar('acc/test_top5', top5.avg, epoch)
+            log_training.write('epoch %d val acc: %f\n'%(epoch, top1.avg))
+
+    return top1.avg
+
+def estimate_grads(trainval_loader, model, criterion, args, epoch, log_training):
+    # switch to train mode
+    model.train()
+    all_grads = []
+    all_targets = []
+    all_preds = []
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    for i, (input, target, target_real, idx) in enumerate(trainval_loader):
+        if args.gpu is not None:
+            input = input.cuda(args.gpu, non_blocking=True)
+        all_targets.append(target)
+        target = target.cuda(args.gpu, non_blocking=True)
+        target_real = target_real.cuda(args.gpu, non_blocking=True)
+        # compute output
+        output, feat = model(input)
+        _, pred = torch.max(output, 1)
+        loss = criterion(output, target).mean()
+        acc1, acc5 = accuracy(output, target_real, topk=(1, 5))
+        top1.update(acc1[0], input.size(0))
+        est_grad = grad(loss, feat)
+        all_grads.append(est_grad[0].detach().cpu().numpy())
+        all_preds.append(pred.detach().cpu().numpy())
+    all_grads = np.vstack(all_grads)
+    all_targets = np.hstack(all_targets)
+    all_preds = np.hstack(all_preds)
+    log_training.write('epoch %d train acc: %f\n'%(epoch, top1.avg))
+    return all_grads, all_targets
 
 # IMPORT MODELS FROM MODEL
-from model.cnn import CNN
 if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
@@ -25,84 +347,16 @@ if __name__ == "__main__":
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
 
-    # load dataset 已经完成图像数据集 对称非对称部分的代码任务
+    # load dataset
     train_dataset, val_dataset, test_dataset, num_classes = get_noisy_dataset(args)
     
     print(train_dataset[0])
-    # # load params 进行coteaching方法获取模型训练参数
-    # input_channel, forget_rate, args.top_bn, args.epoch_decay_start, args.n_epoch = get_dataset_params(args)
+    # # load params
+    input_channel, forget_rate, args.top_bn, args.epoch_decay_start, args.n_epoch = get_dataset_params(args)
+    main()
+
     
-    # # Adjust learning rate and betas for Adam Optimizer 动态调整优化器参数
-    # alpha_plan, beta1_plan = get_optimizer_plan(args=args)
-
-    # # define drop rate schedule 设置训练时的数据集动态丢失率
-    # rate_schedule = get_rate_schedule(args=args, forget_rate=forget_rate)
-
-    # # save result 多方法则调用args.method
-    # save_dir = args.result_dir +'/' +args.dataset+'/coteaching/'
-    # if not os.path.exists(save_dir):
-    #     os.system('mkdir -p %s' % save_dir)
     
-    # model_str=args.dataset+'_coteaching_'+args.noise_type+'_'+str(args.noise_rate)
-    # txtfile=save_dir+"/"+model_str+".txt"
-    # nowTime=datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-    # if os.path.exists(txtfile):
-    #     os.system('mv %s %s' % (txtfile, txtfile+".bak-%s" % nowTime))
-    
-    # # data loader 注意这里去掉最后一个（可能未满）的batch
-    # print(f"loading dataset...{args.dataset}")
-    # noise_or_not = train_dataset.noise_or_not
-    # train_loader = DataLoader(train_dataset, batch_size=args.train_batch_size, drop_last=True, num_workers=args.num_workers, shuffle=True)
-    # val_loader = DataLoader(val_dataset, batch_size=args.test_batch_size, drop_last=True, num_workers=args.num_workers, shuffle=False)
-    # test_loader = DataLoader(test_dataset, batch_size=args.test_batch_size, drop_last=True, num_workers=args.num_workers, shuffle=False)
-
-    # # define model 原论文是选择原始的两个CNN这里我们先模仿，后续添加更多...
-    # print(f"building model...{args.model}")
-    # model1 = CNN(input_channel=input_channel, n_outputs=num_classes)
-    # model1.cuda()
-    # print(model1.parameters)
-    # optimizer1 = torch.optim.Adam(model1.parameters(), lr=args.lr)
-
-    # model2 = CNN(input_channel=input_channel, n_outputs=num_classes)
-    # model2.cuda()
-    # print(model2.parameters)
-    # optimizer2 = torch.optim.Adam(model2.parameters(), lr=args.lr)
-
-    # with open(txtfile, "a") as myfile:
-    #     myfile.write('epoch: train_acc1 train_acc2 test_acc1 test_acc2 pure_ratio1 pure_ratio2\n')
-
-    # mean_pure_ratio1 = 0
-    # mean_pure_ratio2 = 0
-    
-    # epoch=0
-    # train_acc1=0
-    # train_acc2=0
-    # # evaluate models with random weights
-    # test_acc1, test_acc2 = evaluate(test_loader, model1, model2)
-    # print('Epoch [%d/%d] Test Accuracy on the %s test images: Model1 %.4f %% Model2 %.4f %% Pure Ratio1 %.4f %% Pure Ratio2 %.4f %%' % (epoch+1, args.n_epoch, len(test_dataset), test_acc1, test_acc2, mean_pure_ratio1, mean_pure_ratio2))
-    # # save results
-    # with open(txtfile, "a") as myfile:
-    #     myfile.write(str(int(epoch)) + ': '  + str(train_acc1) +' '  + str(train_acc2) +' '  + str(test_acc1) + " " + str(test_acc2) + ' '  + str(mean_pure_ratio1) + ' '  + str(mean_pure_ratio2) + "\n")
-
-    # # training
-    # for epoch in range(1, args.n_epoch):
-    #     # train models
-    #     model1.train()
-    #     adjust_learning_rate(optimizer1, epoch, alpha_plan, beta1_plan)
-    #     model1.train()
-    #     adjust_learning_rate(optimizer2, epoch, alpha_plan, beta1_plan)
-
-    #     print (f"Training f{model_str}...")
-    #     train_acc1, train_acc2, pure_ratio_1_list, pure_ratio_2_list = train(args, train_loader, epoch, model1, optimizer1, model2, optimizer2, rate_schedule, noise_or_not, len(train_dataset))
-    #     # evaluate models
-    #     print (f"Evaluatinging f{model_str}...")
-    #     test_acc1, test_acc2 = evaluate(test_loader, model1, model2)
-    #     # save results
-    #     mean_pure_ratio1 = sum(pure_ratio_1_list)/len(pure_ratio_1_list)
-    #     mean_pure_ratio2 = sum(pure_ratio_2_list)/len(pure_ratio_2_list)
-    #     print('Epoch [%d/%d] Test Accuracy on the %s test images: Model1 %.4f %% Model2 %.4f %%, Pure Ratio 1 %.4f %%, Pure Ratio 2 %.4f %%' % (epoch+1, args.n_epoch, len(test_dataset), test_acc1, test_acc2, mean_pure_ratio1, mean_pure_ratio2))
-    #     with open(txtfile, "a") as myfile:
-    #         myfile.write(str(int(epoch)) + ': '  + str(train_acc1) +' '  + str(train_acc2) +' '  + str(test_acc1) + " " + str(test_acc2) + ' ' + str(mean_pure_ratio1) + ' ' + str(mean_pure_ratio2) + "\n")
 
     
 
--- utils.py
+++ utils.py
@@ -20,8 +20,73 @@ from dataset.Convertype import load_covertype_dataset, Convertype_train, Convert
 # from dataset.Imdb import Imdb, Imdb_train, Imdb_val
 # from dataset.Hepmass import HepmassDataset, Hepmass_train, Hepmass_val
 
+import torch
+import shutil
+import os
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+def prepare_folders(args):
+    """Create log and model folder"""
+    folders_util = [args.root_log, args.root_model,
+                    os.path.join(args.root_log, args.store_name),
+                    os.path.join(args.root_model, args.store_name)]
+    for folder in folders_util:
+        if not os.path.exists(folder):
+            print('creating folder ' + folder)
+            os.mkdir(folder)
+
+def save_checkpoint(args, state, is_best, filename=''):
+    if not filename:
+      filename = '%s/%s/ckpt.pth.tar' % (args.root_model, args.store_name)
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, filename.replace('pth.tar', 'best.pth.tar'))
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, *meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def print(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
 '''
-    获取dataset(is_noise)的工具函数
+    get dataset(is_noise)
 '''
 # split train and val dataset
 def classwise_split(dataset_targets, ratio, num_classes):
@@ -680,3 +745,78 @@ def get_dataset_params(args):
 #     acc1 = 100*float(correct1)/float(total1)
 #     acc2 = 100*float(correct2)/float(total2)
 #     return acc1, acc2
+
+def prepare_folders(args):
+    """Create log and model folder"""
+    folders_util = [args.root_log, args.root_model,
+                    os.path.join(args.root_log, args.store_name),
+                    os.path.join(args.root_model, args.store_name)]
+    for folder in folders_util:
+        if not os.path.exists(folder):
+            print('creating folder ' + folder)
+            os.mkdir(folder)
+
+def save_checkpoint(args, state, is_best, filename=''):
+    if not filename:
+      filename = '%s/%s/ckpt.pth.tar' % (args.root_model, args.store_name)
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, filename.replace('pth.tar', 'best.pth.tar'))
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, *meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def print(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
