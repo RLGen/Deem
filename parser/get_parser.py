a7455f7d6b1a07d0db35cf2ebb9f12b25214ec2e
jkais-jks <147993417+RLGen@users.noreply.github.com>
2024-10-24 01:02:12 +0800

    .

--- parser/get_parser.py
+++ parser/get_parser.py
@@ -2,14 +2,13 @@ import argparse
 
 def get_parser():
     parser = argparse.ArgumentParser(description="Detecting Errors in Dataset Labels")
-    parser.add_argument("--data_root", type=str, default="/data2/wyh-dataset/tabular-dataset", help="Path to the dataset root folder")
+    parser.add_argument("--data_root", type=str, default="tabular-dataset", help="Path to the dataset root folder")
     parser.add_argument("--dataset", type=str, choices=["cifar10", "mnist", "kmnist", "cifar100", "food101", "clothing-1m",
                                                          "adults", "convertype", "drybean", "news", "imdb","hepmass", "svhn"], default="hepmass", help="Dataset to use")
     parser.add_argument("--noise_type", type=str, choices=["sym_noise", "asymmetric_noise"], default="sym_noise", help="the type of noise")
     parser.add_argument('--noise_rate', type = float, default = 0.2, help = 'corruption rate, should be less than 1')
     parser.add_argument('--train_ratio', type=float, default=0.9, help='Training data ratio')
-    parser.add_argument("--method", type=str, choices=["coteaching"], default="coteaching", help="Method for detecting errors")
-    parser.add_argument('--result_dir', type = str, default = '/home/wangyuhui/coteaching/result', help = 'dir to save result txt files')
+    parser.add_argument('--result_dir', type = str, default = 'result', help = 'dir to save result txt files')
     
     # train model
     parser.add_argument("--model", type=str,choices=["cnn"], default="cnn", help="model for training")
@@ -28,5 +27,6 @@ def get_parser():
     parser.add_argument('--num_workers', type=int, default=4, help='how many subprocesses to use for data loading')
     parser.add_argument('--num_iter_per_epoch', type=int, default=400)
     parser.add_argument('--epoch_decay_start', type=int, default=80)
-    parser.add_argument('--coreset_size', type=float, default=0.2)
+    parser.add_argument('--K', type=float, default=0.2)
+    parser.add_argument('--tau', type=float, default=0.2)
     return parser
